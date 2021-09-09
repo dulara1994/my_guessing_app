cc1ea22cae6e531e94497bbadd5071d5905b5764
Dulara M. Colombage <dularamalindu@gmail.com>
2021-09-09 15:06:09 +0530

    initial-commit

---
+++ src/containers/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainPageWrapper = styled.div``;
+
+const main = () => {
+  return <MainPageWrapper></MainPageWrapper>;
+};
+
+export default main;
